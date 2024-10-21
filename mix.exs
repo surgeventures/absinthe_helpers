ab995524c7466c95975e61280f7de46f4809e098
Burak Kaymakci <burak.kaymakci@fresha.com>
2024-10-21 12:13:28 +0200

    Bump package version from 0.1.7 to 0.1.8

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AbsintheHelpers.MixProject do
   def project do
     [
       app: :absinthe_helpers,
-      version: "0.1.7",
+      version: "0.1.8",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
